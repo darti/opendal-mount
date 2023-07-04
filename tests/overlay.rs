5a42ad693fdd83b6f9ef93b9562132fd8928823f
Matthieu Dartiguenave <matthieu.dartiguenave@gmail.com>
2023-07-04 22:00:53 +0900

    Add test

--- tests/overlay.rs
+++ tests/overlay.rs
@@ -82,3 +82,77 @@ async fn test_base_only() -> anyhow::Result<()> {
     assert_eq!(entries, &["hello.txt"]);
     Ok(())
 }
+
+#[derive(Debug, Clone)]
+struct UnionPolicy {}
+
+#[async_trait]
+impl Policy for UnionPolicy {
+    fn owner<B, O>(
+        &self,
+        _base_info: B,
+        _overlay_info: O,
+        _path: &str,
+        _op: PolicyOperation,
+    ) -> Source
+    where
+        B: FnOnce() -> AccessorInfo,
+        O: FnOnce() -> AccessorInfo,
+    {
+        Source::Base
+    }
+
+    fn capability<B, O>(&self, base_info: B, _overlay_info: O) -> Capability
+    where
+        B: FnOnce() -> AccessorInfo,
+        O: FnOnce() -> AccessorInfo,
+    {
+        base_info().capability()
+    }
+
+    async fn next<B: Page, O: Page>(
+        &self,
+        base: &mut B,
+        overlay: &mut O,
+    ) -> opendal::Result<Option<Vec<oio::Entry>>> {
+        let entries = overlay.next().await?;
+        if let Some(_) = entries {
+            return Ok(entries);
+        } else {
+            let entries = base.next().await;
+
+            entries
+        }
+    }
+}
+
+#[tokio::test]
+async fn test_union() -> anyhow::Result<()> {
+    let mut base_builder = Fs::default();
+    base_builder.root(BASE_ROOT);
+
+    let mut overlay_builder = Fs::default();
+    overlay_builder.root(OVERLAY_ROOT);
+
+    let mut builder = Overlay::default();
+
+    builder
+        .base_builder(base_builder)
+        .overlay_builder(overlay_builder)
+        .policy(UnionPolicy {});
+
+    let op = Operator::new(builder)?.finish();
+
+    let ds = op.list("/").await?;
+    let mut entries: Vec<String> = ds
+        .into_stream()
+        .map_ok(|e| e.name().to_owned())
+        .try_collect()
+        .await?;
+
+    entries.sort();
+
+    assert_eq!(entries, &["hello.txt", "world.txt"]);
+
+    Ok(())
+}
